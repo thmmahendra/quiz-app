a96f519440ca582a40279fb01264b197a001ce00
thmmahendra <thmmahendra@gmail.com>
2023-07-11 22:59:44 +0545

    Initial Commit

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
 const questions = [
     {
         question: "Which is the largest animal in the world",
-        answer: [
-            { tex: "Shark", correct: false },
-            { tex: "Blue Whale", correct: true },
-            { tex: "Elephant", correct: false },
-            { tex: "Girraffe", correct: false },
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Girraffe", correct: false },
         ]
     },
     {
         question: "Which is the smallest continet in the world",
-        answer: [
-            { tex: "Asia", correct: false },
-            { tex: "Australia", correct: true },
-            { tex: "Arctic", correct: false },
-            { tex: "Africa", correct: false },
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
         ]
     },
     {
         question: "Which is the largest desert in the world",
-        answer: [
-            { tex: "Kalahhari", correct: false },
-            { tex: "Gobi", correct: false },
-            { tex: "Sahara", correct: false },
-            { tex: "Antartica", correct: true },
+        answers: [
+            { text: "Kalahhari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Antartica", correct: true },
         ]
     },
     {
         question: "Which is the smallest country in the world",
-        answer: [
-            { tex: "Monaco", correct: false },
-            { tex: "Maldives", correct: false },
-            { tex: "Vatican City", correct: true },
-            { tex: "Tonga", correct: false },
+        answers: [
+            { text: "Monaco", correct: false },
+            { text: "Maldives", correct: false },
+            { text: "Vatican City", correct: true },
+            { text: "Tonga", correct: false },
         ]
     }
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+
+startQuiz();
